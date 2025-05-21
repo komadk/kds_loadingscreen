234874dc6675f84d8aa850911dfd889f78df5e3c
Komá <maltheeliasj@gmail.com>
2025-05-21 21:00:02 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+-- This resource is part of the default Cfx.re asset pack (cfx-server-data)
+-- Altering or recreating for local use only is strongly discouraged.
+
+fx_version 'cerulean'
+game 'gta5'
+
+author 'KD-Scripts'
+description 'Custom loadingscreen made by KD-Scripts'
+version '1.0.0'
+
+files {
+    'index.html',
+    'bankgothic.ttf',
+    'loadscreen.png'
+}
+
+loadscreen 'index.html'
